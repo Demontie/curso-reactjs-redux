68026a745871ff86f107700af763d111c9a980ae
Demontie costa <demontiecosta17@gmail.com>
2018-08-20 00:41:46 -0300

    comentarios adicionados para melhor entendimento

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import CounterReducer from './CounterReducer';
+//Quando definimos um novo reducers utilizando combinerecers é extremamente necessario colocar um nome facil de entender o reducers, porque o reducer tem a responsabilidade de executar uma logica que vai associar um novo estado(state) aquele componete
 export default combineReducers({
     //data:dateReducer
     counter: CounterReducer    
